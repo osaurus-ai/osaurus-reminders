d3095960daa417af28126b10bf088ea866bfe452
Terence Pae <terencepae@gmail.com>
2026-07-23 23:22:15 -0400

    Release 1.1.0: reliability audit fixes and shared SDK adoption

--- Sources/osaurus_reminders/Plugin.swift
+++ Sources/osaurus_reminders/Plugin.swift
@@ -632,7 +632,7 @@ let remindersManifestJSON: String = {
     {
       "plugin_id": "osaurus.reminders",
       "name": "Reminders",
-      "version": "1.0.4",
+      "version": "1.1.0",
       "description": "An Osaurus plugin for interacting with macOS Reminders via EventKit.",
       "license": "MIT",
       "authors": ["Osaurus"],
